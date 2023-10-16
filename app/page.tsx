7ef6f1ab0181cbda40ab394d5325e387dce530e9
fGiordi <giordifungula@gmail.com>
2023-10-16 19:33:37 +0200

    base top part of form code added

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import EmployeeList from "./components/EmployeeList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-center p-24">
       {/* TABLE will go here */}
     <EmployeeList/>
       <div className='flex flex-col w-full'>
-        <div className='p-5 flex items-center justify-center'>
-          <h2 className='text-gray-800 font-bold'>Employee Information</h2>
+        <div className='py-5 flex flex-col justify-center border-2 border-black w-full'>
+          <div className="flex items-center justify-center">
+          <h2 className='text-gray-800 font-bold p-5 text-center'>Employee Information</h2>
+
+          </div>
+          <div className="px-5 flex justify-end space-x-6 max-w-[700px]">
+            <button className="text-center px-3 text-[16px] text-black rounded-md bg-gray-200">Cancel</button>
+            {/* TODO to change color of Save button based on selected color from DB */}
+            <button className="text-center px-3 text-[16px] text-white rounded-md bg-blue-500">Save</button>
+          </div>
         </div>
       </div>
     </main>
